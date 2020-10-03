c94b5a57e570ffe8ee5a4352493807cc2a596d5d
EduardoNave <emonterrosa.nave@gmail.com>
2020-10-03 00:32:12 -0600

    componentes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,26 @@
 import { NgModule } from '@angular/core';
+
+// Required services for navigation
 import { Routes, RouterModule } from '@angular/router';
 
+// Import all the components for which navigation service has to be activated
+import { SignInComponent } from '././components/sign-in/sign-in.component';
+import { SignUpComponent } from '././components/sign-up/sign-up.component';
+import { LobbyComponent } from '././components/lobby/lobby.component';
+import { TicketsComponent } from '././components/tickets/tickets.component';
+import { ForgotPasswordComponent } from '././components/forgot-password/forgot-password.component';
+//import { AuthGuard } from "././guard/auth.guard";
+import { VerifyEmailComponent } from '././components/verify-email/verify-email.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: '/sign-in', pathMatch: 'full' },
+ { path: 'sign-in', component: SignInComponent },
+ { path: 'register-user', component: SignUpComponent },
+ { path: 'lobby', component: LobbyComponent },
+ { path: 'tickets', component: TicketsComponent },
+ { path: 'forgot-password', component: ForgotPasswordComponent },
+ { path: 'verify-email-address', component: VerifyEmailComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,36 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LobbyComponent } from './components/lobby/lobby.component';
+import { SignInComponent } from './components/sign-in/sign-in.component';
+import { SignUpComponent } from './components/sign-up/sign-up.component';
+import { ForgotPasswordComponent } from './components/forgot-password/forgot-password.component';
+import { VerifyEmailComponent } from './components/verify-email/verify-email.component';
+import { TicketsComponent } from './components/tickets/tickets.component';
+
+// Firebase services + enviorment module
+import { AngularFireModule } from "@angular/fire";
+import { AngularFireAuthModule } from "@angular/fire/auth";
+import { AngularFirestoreModule } from '@angular/fire/firestore';
+import { environment } from '../environments/environment';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LobbyComponent,
+    SignInComponent,
+    SignUpComponent,
+    ForgotPasswordComponent,
+    VerifyEmailComponent,
+    TicketsComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AngularFireModule.initializeApp(environment.firebase),
+    AngularFireAuthModule,
+    AngularFirestoreModule
   ],
   providers: [],
   bootstrap: [AppComponent]
