6646608e9e0d94b5a0be87e4e3492d7a6992b07a
EduardoNave <emonterrosa.nave@gmail.com>
2020-10-03 00:16:12 -0600

    toastr y firebase instalados

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyBxMEK2l-_Dmt4YrQDYBaOMv2GeXARxQMo",
+    authDomain: "veterinaria-lissette-d05fa.firebaseapp.com",
+    databaseURL: "https://veterinaria-lissette-d05fa.firebaseio.com",
+    projectId: "veterinaria-lissette-d05fa",
+    storageBucket: "veterinaria-lissette-d05fa.appspot.com",
+    messagingSenderId: "177426068971",
+    appId: "1:177426068971:web:3bee7c958dc52d3fcb95ae",
+    measurementId: "G-W53SFENPFR"
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,17 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyBxMEK2l-_Dmt4YrQDYBaOMv2GeXARxQMo",
+    authDomain: "veterinaria-lissette-d05fa.firebaseapp.com",
+    databaseURL: "https://veterinaria-lissette-d05fa.firebaseio.com",
+    projectId: "veterinaria-lissette-d05fa",
+    storageBucket: "veterinaria-lissette-d05fa.appspot.com",
+    messagingSenderId: "177426068971",
+    appId: "1:177426068971:web:3bee7c958dc52d3fcb95ae",
+    measurementId: "G-W53SFENPFR"
+  }
 };
 
 /*
