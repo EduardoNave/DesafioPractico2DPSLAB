f83644153ef65d6561fd29ee973479b91841f4a5
EduardoNave <emonterrosa.nave@gmail.com>
2020-10-03 00:37:27 -0600

    clase user

---
+++ src/app/models/user.ts
@@ -0,0 +1,13 @@
+export class User {
+    uid: string;
+    email: string;
+    displayName: string;
+    photoURL: string;
+    emailVerified: boolean;
+    dui: string;
+    petName: string;
+    tratamiento: string;
+    medicamento: string;
+    costo: number;
+    visitas: number;
+}
